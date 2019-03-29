ce4a765eaed88b7f1a8d473a08121c3ce8bfaeda
tal.regev <tal.regev@gmail.com>
2019-03-29 08:05:08 +0300

    remove old assets of gazebo update setup.py add pioneer3at-circle world

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 import sys, os.path
 
 # Don't import gym module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym-gazebo'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym-webots'))
 
-setup(name='gym-gazebo',
-      version='0.0.2',
+setup(name='gym-webots',
+      version='0.0.1',
       packages=find_packages(),
-      install_requires=['gym>=0.2.3'],
-      description='The OpenAI Gym for robotics: A toolkit for developing and comparing your reinforcement learning agents using Gazebo and ROS.',
-      url='https://github.com/erlerobot/gym',
-      author='Erle Robotics',
-      package_data={'gym-gazebo': ['envs/assets/launch/*.launch', 'envs/assets/worlds/*']},
+      install_requires=['gym>=0.12'],
+      description='The OpenAI Gym for robotics: A toolkit for developing and comparing your reinforcement learning agents using webots and ROS.',
+      url='https://github.com/talregev/gym-webots',
+      author='Tal Regev',
+      package_data={'gym-webots': ['envs/assets/*']},
 )
