f1272c5a5e36c197d6c5e0a413e3e65319bc04a6
tal.regev <tal.regev@gmail.com>
2019-03-29 10:25:49 +0300

    update to webots env

--- gym_webots/envs/gazebo_env.py
+++ gym_webots/envs/gazebo_env.py
@@ -10,30 +10,31 @@ from std_srvs.srv import Empty
 import random
 from rosgraph_msgs.msg import Clock
 
-class GazeboEnv(gym.Env):
+class WebotsEnv(gym.Env):
     """Superclass for all Gazebo environments.
     """
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, launchfile):
+    def __init__(self, worldfile):
         self.last_clock_msg = Clock()
 
         random_number = random.randint(10000, 15000)
         # self.port = "11311"#str(random_number) #os.environ["ROS_PORT_SIM"]
-        # self.port_gazebo = "11345"#str(random_number+1) #os.environ["ROS_PORT_SIM"]
+        # self.port_webots = "11345"#str(random_number+1) #os.environ["ROS_PORT_SIM"]
         self.port = str(random_number) #os.environ["ROS_PORT_SIM"]
-        self.port_gazebo = str(random_number+1) #os.environ["ROS_PORT_SIM"]
+        self.port_webots = str(random_number+1) #os.environ["ROS_PORT_SIM"]
 
         os.environ["ROS_MASTER_URI"] = "http://localhost:"+self.port
-        os.environ["GAZEBO_MASTER_URI"] = "http://localhost:"+self.port_gazebo
+        #os.environ["Webots stream"] = "http://localhost:"+self.port_webots
         #
         # self.ros_master_uri = os.environ["ROS_MASTER_URI"];
 
         print("ROS_MASTER_URI=http://localhost:"+self.port + "\n")
-        print("GAZEBO_MASTER_URI=http://localhost:"+self.port_gazebo + "\n")
+        print("Webots stream=http://localhost:"+self.port_webots + "\n")
 
         # self.port = os.environ.get("ROS_PORT_SIM", "11311")
-        ros_path = os.path.dirname(subprocess.check_output(["which", "roscore"]))
+        roscore_path = os.path(subprocess.check_output(["which", "roscore"]))
+        webots_path = os.path(subprocess.check_output(["which", "Webots"]))
 
         # NOTE: It doesn't make sense to launch a roscore because it will be done when spawing Gazebo, which also need
         #   to be the first node in order to initialize the clock.
@@ -42,18 +43,23 @@ class GazeboEnv(gym.Env):
         # time.sleep(1)
         # print ("Roscore launched!")
 
+        
 
-        if launchfile.startswith("/"):
+        if worldfile.startswith("/"):
             fullpath = launchfile
         else:
-            fullpath = os.path.join(os.path.dirname(__file__), "assets", "launch", launchfile)
+            fullpath = os.path.join(os.path.dirname(__file__), "assets", "worlds", worldfile)
+
         if not os.path.exists(fullpath):
             raise IOError("File "+fullpath+" does not exist")
 
-        self._roslaunch = subprocess.Popen([sys.executable, os.path.join(ros_path, b"roslaunch"), "-p", self.port, fullpath])
-        print ("Gazebo launched!")
+        self._roscore = subprocess.Popen([sys.executable, roscore_path, "-p", self.port, fullpath])
+        port_param = '--stream="port=' + self.port_webots + '"'
+        self._webots = subprocess.Popen([sys.executable, webots_path, "--batch", "--no-sandbox", "--stdout", "--stderr", port_param,  fullpath])
 
-        self.gzclient_pid = 0
+        print ("Webots launched!")
+
+        self.webots_pid = 0
 
         # Launch the simulation with the given launchfile name
         rospy.init_node('gym', anonymous=True)
@@ -101,40 +107,37 @@ class GazeboEnv(gym.Env):
 
         if close:
             tmp = os.popen("ps -Af").read()
-            proccount = tmp.count('gzclient')
+            proccount = tmp.count('webots')
             if proccount > 0:
-                if self.gzclient_pid != 0:
-                    os.kill(self.gzclient_pid, signal.SIGTERM)
+                if self.webots_pid != 0:
+                    os.kill(self.webots_pid, signal.SIGTERM)
                     os.wait()
             return
 
         tmp = os.popen("ps -Af").read()
-        proccount = tmp.count('gzclient')
+        proccount = tmp.count('webots')
         if proccount < 1:
-            subprocess.Popen("gzclient")
-            self.gzclient_pid = int(subprocess.check_output(["pidof","-s","gzclient"]))
+            subprocess.Popen("webots")
+            self.webots_pid = int(subprocess.check_output(["pidof","-s","webots"]))
         else:
-            self.gzclient_pid = 0
+            self.webots_pid = 0
 
     def _close(self):
 
-        # Kill gzclient, gzserver and roscore
+        # Kill webots and roscore
         tmp = os.popen("ps -Af").read()
-        gzclient_count = tmp.count('gzclient')
-        gzserver_count = tmp.count('gzserver')
+        webots_count = tmp.count('webots')
         roscore_count = tmp.count('roscore')
         rosmaster_count = tmp.count('rosmaster')
 
-        if gzclient_count > 0:
-            os.system("killall -9 gzclient")
-        if gzserver_count > 0:
-            os.system("killall -9 gzserver")
+        if webots_count > 0:
+            os.system("killall -9 webots")
         if rosmaster_count > 0:
             os.system("killall -9 rosmaster")
         if roscore_count > 0:
             os.system("killall -9 roscore")
 
-        if (gzclient_count or gzserver_count or roscore_count or rosmaster_count >0):
+        if (webots_count or roscore_count or rosmaster_count >0):
             os.wait()
 
     def _configure(self):
